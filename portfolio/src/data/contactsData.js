4e8dd2e989cbf22702c7b2769ef6ec11308bf2af
Benjamin Glass <Benjamin.glass18@gmail.com>
2026-06-21 11:48:41 +0200

    first commit

---
+++ portfolio/src/data/contactsData.js
@@ -0,0 +1,7 @@
+export const contactsData = {
+    email: 'benjamin.glass18@gmail.com',
+    phone: '',
+    address: '',
+
+    sheetAPI: ''
+}
---
+++ portfolio/src/data/socialsData.js
@@ -0,0 +1,14 @@
+export const socialsData = {
+    github: 'https://github.com/glassben',
+    facebook: '',
+    linkedIn: 'https://www.linkedin.com/in/benjamin-glass-9532b4249',
+    instagram: '',
+    codepen: '',
+    twitter: '',
+    reddit: '',
+    blogger: '',
+    medium: '',
+    stackOverflow: '',
+    gitlab: 'https://gitlab.com/benjamin.glass18/portfolio',
+    youtube: ''
+}
